38d5f60629ffa7d915e62cde77c915bc53d73018
Mikkel J <appwingman@gmail.com>
2011-09-16 23:46:47 +0200

    Adding chrome on dat shizzle

--- gen/com/wingman/R.java
+++ gen/com/wingman/R.java
@@ -14,26 +14,25 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050013;
-        public static final int button2=0x7f050014;
-        public static final int button3=0x7f050015;
+        public static final int button1=0x7f050012;
+        public static final int button2=0x7f050013;
+        public static final int button3=0x7f050014;
         public static final int buttonCancel=0x7f05000b;
         public static final int buttonOk=0x7f05000a;
         public static final int displaydescriptionselectionad=0x7f05000c;
         public static final int editText1=0x7f050001;
         public static final int editText2=0x7f050002;
         public static final int editText3=0x7f050003;
-        public static final int facerecad=0x7f05000f;
-        public static final int fullscreenlayout=0x7f050011;
+        public static final int fullscreenlayout=0x7f050010;
         public static final int imageView1=0x7f05000d;
         public static final int linearLayout1=0x7f050000;
         public static final int linearLayout2=0x7f050008;
         public static final int linearLayout3=0x7f050009;
         public static final int linearLayout4=0x7f050004;
-        public static final int personad=0x7f050012;
+        public static final int personad=0x7f050011;
         public static final int progressBar1=0x7f05000e;
         public static final int ratingBar1=0x7f050007;
-        public static final int surface_camera=0x7f050010;
+        public static final int surface_camera=0x7f05000f;
         public static final int textView1=0x7f050005;
         public static final int toggleButton1=0x7f050006;
     }
--- src/com/wingman/FaceRecActivity.java
+++ src/com/wingman/FaceRecActivity.java
@@ -45,18 +45,7 @@ public class FaceRecActivity extends Activity implements OnTouchListener{
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.facerec);
 		
-		// Create the adView
-	    adView = new AdView(this, AdSize.BANNER, MY_AD_UNIT_ID);
-
-	    // Lookup your LinearLayout assuming it’s been given
-	    // the attribute android:id="@+id/mainLayout"
-	    LinearLayout layout = (LinearLayout) findViewById(R.id.facerecad);
-
-	    // Add the adView to it
-	    layout.addView(adView);
-
-	    // Initiate a generic request to load it with an ad
-	    adView.loadAd(new AdRequest());
+		
 		
 		// Setup touch listener, to detect x,y coordinates of touch events
 		ImageView image = (ImageView) findViewById(R.id.imageView1);
